e01cfc93f0e28ff120676328466206e5158c1abf
santhoshparakunnath <santhosh.parakunnath@gmail.com>
2026-07-28 19:46:32 +0530

    Create business_verticals.sql

---
+++ database/seeds/business_verticals.sql
@@ -0,0 +1,8 @@
+INSERT INTO business_verticals (name, description)
+VALUES
+('Spices','Spices and seasonings'),
+('Tea','Tea exporters and buyers'),
+('Coffee','Coffee exporters and buyers'),
+('Food Ingredients','Food ingredient suppliers'),
+('Herbal Products','Herbal and botanical products'),
+('Seafood','Seafood exporters and importers');
