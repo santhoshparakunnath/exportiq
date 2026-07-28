3a60dc11fc1f0c4c6d5d69e3a9c88759dafdc5a1
santhoshparakunnath <santhosh.parakunnath@gmail.com>
2026-07-28 20:38:04 +0530

    Add certifications seed data
    Insert multiple certification records into the database.

---
+++ database/seeds/006_certifications_seed.sql
@@ -0,0 +1,24 @@
+INSERT INTO certifications
+(name, short_name, issuing_authority, scope)
+
+VALUES
+
+('ISO 9001','ISO 9001','International Organization for Standardization','General'),
+
+('ISO 22000','ISO 22000','International Organization for Standardization','Food'),
+
+('HACCP','HACCP','Codex Alimentarius','Food'),
+
+('BRCGS Food Safety','BRCGS','BRCGS','Food'),
+
+('Organic','Organic','Various Accredited Bodies','Agriculture'),
+
+('Fairtrade','Fairtrade','Fairtrade International','Agriculture'),
+
+('Rainforest Alliance','Rainforest','Rainforest Alliance','Agriculture'),
+
+('Halal','Halal','Various Certification Bodies','Food'),
+
+('Kosher','Kosher','Various Certification Bodies','Food'),
+
+('FSSC 22000','FSSC','Foundation FSSC','Food');
